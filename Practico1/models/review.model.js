4c764c65a052c4ff5f0d45e22e1621089fe8fb06
favio-ballon <favioballon@gmail.com>
2024-09-02 20:04:55 -0400

    .

--- Practico1/models/review.model.js
+++ Practico1/models/review.model.js
@@ -1,4 +1,4 @@
-module.export = (sequelize, Sequelize) => {
+module.exports = (sequelize, Sequelize) => {
     const Review = sequelize.define("review", {
         comentario: {
             type: Sequelize.STRING,
